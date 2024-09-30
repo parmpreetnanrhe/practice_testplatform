c39c1e13205b748b96e2fbed702a53aace24a975
Arshdeep Singh <arshdeep.singh@tcyonline.org>
2024-09-30 17:13:40 +0530

    arsh changes

--- src/components/Pallet.js
+++ src/components/Pallet.js
@@ -22,12 +22,17 @@ export default function Pallet() {
       <div className={`side-panel ${isPalletOpen ? 'open' : ''}`}>
         <div className="select-pallet-tag">
           <SelectComponent
+            options={['Section 1', 'Section 2']}
+            onSelectChange=""
+            defaultText="Please select a value"
+          />
+          <SelectComponent
             options={['All', 'Correct', 'Incorrect', 'Un-attempted', 'Flagged']}
             onSelectChange=""
             defaultText="Please select a value"
           />
         </div>
-        <div className='questtion-box-container'>
+        <div className='question-box-container'>
             <div qid="1" className='ques-box'><span>1</span></div>
             <div qid="2" className='ques-box'><span>2</span></div>
             <div qid="3" className='ques-box'><span>3</span></div>
